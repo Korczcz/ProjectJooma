6d7db03dde49b6942ef0ac0bd1d9b30400d1a484
Korczcz <kapiwlkp33@gmail.com>
2023-06-06 11:21:23 +0200

    added third page

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-06-06 05:54:10 UTC
+#Software: Joomla! 4.3.2 Stable [ Bora ] 30-May-2023 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-06-06T05:54:10+00:00	INFO ::1	joomlafailure	Empty password not allowed.
